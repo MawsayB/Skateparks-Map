c39292c230dcc2be831f29dcf565d7cfe97aa3aa
Marcy Bursac <msbursac@gmail.com>
2018-10-07 18:26:30 -0500

    initial commit

---
+++ Map.js
@@ -0,0 +1,14 @@
+/* jshint esnext: true */
+/* jshint ignore:start */
+
+import React from 'react';
+
+class Map extends React.Component {
+    render() {
+        return (
+
+        );
+    }
+}
+/* jshint ignore:end */
+export default Map;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+/* jshint esnext: true */
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="container">
+        {/* TODO: where Search will go */}
+        <div class="options-box">
+          <h1>Wanna Skateboard?</h1>
+        </div>
+        {/* TODO: where Map will go */}
+        <div id="map"></div>
       </div>
     );
   }
