06f5ab9c21b30262b691920681dad319e540f12f
Marcy Bursac <msbursac@gmail.com>
2018-10-08 21:18:33 -0500

    renders initial Google Map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 /* Google Maps API designed using: */
-/* https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/#the-map-container-component */
+/* https://www.npmjs.com/package/google-maps-react */
 
 /* jshint esnext: true */
 /* jshint ignore:start */
 
 import React, { Component } from 'react';
-import Map from './Map';
-import MapContainer from './MapContainer';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class App extends Component {
+
   render() {
     return (
-      <div class="container">
-        {/* TODO: where Search will go */}
-        <div class="options-box">
-          <h1>Wanna Skateboard?</h1>
-        </div>
-        <MapContainer />
+      <div className="App">
+        <Map
+          google={this.props.google}
+          zoom={15}
+          initialCenter={{
+            lat: 38.788105, lng: -90.497437
+          }}
+          onClick={this.onMapClicked}
+        >
+        </Map>
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyAeCKdS_7fB9UBZ1qaVUw8BRLl5VCuTzCY')
+})(App)
